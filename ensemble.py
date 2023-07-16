need to fix perplexity computation

ensemble.py
@@ -19,7 +19,7 @@ class Ensemble():
         self.model_dirs = model_dirs
         self.lora_ensemble = 0 
         self.num_ensemble = len(self.model_dirs)
-        self.lambdas = [1 for _ in range(self.num_ensemble)]
+        self.lambdas = [3/4 for _ in range(self.num_ensemble)]
         self.indiv_eps = [0 for _ in range(self.num_ensemble)]
 
         for i, dir in enumerate(self.model_dirs):
@@ -65,6 +65,13 @@ class Ensemble():
 
     def priv_pred(self, output_dists):
         cur_eps = []
+        #for i in range(self.num_ensemble):
+            #self.lambdas[i] = min(1, self.sigma_2 * self.num_ensemble / np.sqrt(
+            #                                    torch.sum(output_dists[i]**2)) * np.sqrt(
+            #                                                (2 * self.eps) /
+            #                                                (self.alpha * self.q_budget)
+            #                                                )
+            #)
         for i in range(self.num_ensemble):
             cur_eps.append(self.calc_privacy_loss(output_dists[i], i))
             if self.indiv_eps[i] > self.eps:
@@ -77,21 +84,13 @@ class Ensemble():
                                                    (1 - self.lambdas[i]) *
                                                    output_dists[self.num_ensemble]
                                                    )
-        ensemble_dist +=  np.random.normal(0, self.sigma_2, dim_size)
+        #ensemble_dist +=  np.random.normal(0, self.sigma_2, dim_size)
         # This still gets negative numbers
-        ensemble_dist += 2 * torch.min(ensemble_dist) 
-        ensemble_dist /= torch.sum(ensemble_dist) 
+        #ensemble_dist += 2 * torch.min(ensemble_dist) 
+        #ensemble_dist /= torch.sum(ensemble_dist) 
 
         for i in range(self.num_ensemble):
             self.indiv_eps[i] += cur_eps[i] if self.lambdas[i] != 0 else 0 
-
-        for i in range(self.num_ensemble):
-            self.lambdas[i] = self.sigma_2 * self.num_ensemble / np.sqrt(
-                                                torch.sum(output_dists[i]**2)) * np.sqrt(
-                                                            (2 * self.eps) /
-                                                            (self.alpha * self.q_budget)
-                                                            )
-        
         return ensemble_dist
 
     def reg_pred(self, output_dists):
@@ -103,3 +102,8 @@ class Ensemble():
     def print_priv_budgets(self):
         for budg in self.indiv_eps:
             print(budg)
+        return
+    
+    def print_lambdas(self):
+        for lambd in self.lambdas:
+            print(lambd.item())

main.py
@@ -15,7 +15,7 @@ parser.add_argument("--model_name", type=str, default="GPT2")
 parser.add_argument("--data_subset", type=str, default="wikitext-103-raw-v1")
 parser.add_argument("--device", type=str, default="cuda:7")
 parser.add_argument("--seq_length", type=int, default=512)
-parser.add_argument("--query_budget", type=int, default=1024)
+parser.add_argument("--query_budget", type=int, default=1048)
 
 
 def main():
@@ -32,29 +32,52 @@ def main():
     seq_length = 526
     corpus = load_wikitext()
     val_loader = DataLoader(CorpusDataset(corpus['valid'], seq_length))
-    losses = []
+    priv_losses = []
+    pub_losses = []
+    fine_tuned_losses = []
     with torch.no_grad():
         for i, data in enumerate(tqdm.tqdm(val_loader)):
             metric = Perplexity()
             data = data.to(args.device)
             output_dists = priv_ensemble.pred_dist(data)
+            pub_dists = pub_model(data).logits.squeeze().to('cpu')
+
             priv_dists = []
+            fine_tuned_dists = []
             for j in range(seq_length):
-                #priv_dist =priv_ensemble.reg_pred([output_dist[j]
-                #                                   for output_dist in output_dists])
-                priv_dist = priv_ensemble.priv_pred([output_dist[j]
-                                                     for output_dist in output_dists])
+                inp = [output_dist[j] for output_dist in output_dists]
+                fine_tuned_dist = priv_ensemble.reg_pred(inp)
+                priv_dist = priv_ensemble.priv_pred(inp)
+                
                 priv_dists.append(torch.log(priv_dist))
-            #priv_dists = torch.stack(priv_dists).unsqueeze(0)
-            priv_dists = torch.stack(priv_dists)
-            #labels = data.squeeze(0).unsqueeze(-1)
-            #metric.update(priv_dists, data.to('cpu'))
-            #losses.append(metric.compute().item())
-            losses.append(XHeval(priv_dists, data.to('cpu')))
+                fine_tuned_dists.append(torch.log(fine_tuned_dist))
+
+            priv_dists = torch.stack(priv_dists).unsqueeze(0)
+            fine_tuned_dists = torch.stack(fine_tuned_dists).unsqueeze(0)
+            pub_dists = pub_dists.unsqueeze(0)
+
+            labels = data.squeeze(0).unsqueeze(-1)
+            metric.update(priv_dists, data.to('cpu'))
+            priv_losses.append(metric.compute().item())
+            metric.update(pub_dists, data.to('cpu'))
+            pub_losses.append(metric.compute().item())
+            metric.update(fine_tuned_dists, data.to('cpu'))
+            fine_tuned_losses.append(metric.compute().item())
+
+            #priv_dists = torch.stack(priv_dists)
+            #fine_tuned_dists = torch.stack(fine_tuned_dists)
+
+            #priv_losses.append(XHeval(priv_dists, data.to('cpu')))
+            #fine_tuned_losses.append(XHeval(fine_tuned_dists, data.to('cpu')))
+            #pub_losses.append(XHeval(pub_dists, data.to('cpu')))
+            #priv_ensemble.print_priv_budgets()
+
             if i >= int(args.query_budget / seq_length):
                 break
-        print(f"Val Loss: {np.mean(losses):.4f} ")
-    priv_ensemble.print_priv_budgets()
+
+        print(f"Ensemble Val Loss: {np.mean(priv_losses):.4f} ")
+        print(f"Fine-Tuned Val Loss: {np.mean(fine_tuned_losses):.4f} ")
+        print(f"Pre-Trained Val Loss: {np.mean(pub_losses):.4f} ")
 
 def load_wikitext():
     corpus = dict()